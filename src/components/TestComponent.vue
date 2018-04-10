2dd74a7cfd97f331ce004cd9b2df4e58b7be3688
John Nicholas <themrtortoise@gmail.com>
2018-04-10 13:17:23 +0100

    sub component

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data () {
+    return {
+      msg: 'wtf yo - test complete'
+    }
+  }
+}
+</script>
