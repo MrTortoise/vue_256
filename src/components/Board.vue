3901e59fca5bcfae036bf7f30217392af7091d80
John Nicholas <themrtortoise@gmail.com>
2018-04-18 16:14:42 +0100

    handling keyboard input - not unhandling it though!

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="boardWrapper">
-    <div class="r0c0" /><div class="r0c1" /><div class="r0c2" /><div class="r0c3" />
+  <div
+    class="boardWrapper"
+  >
+    <div
+      class="r0c0"
+      @keyup.65="up" />{{ x }},{{ y }}<div class="r0c1" /><div class="r0c2" /><div class="r0c3" />
     <div class="r1c0" /><div class="r1c1" /><div class="r1c2" /><div class="r1c3" />
     <div class="r2c0" /><div class="r2c1" /><div class="r2c2" /><div class="r2c3" />
     <div class="r3c0" /><div class="r3c1" /><div class="r3c2" /><div class="r3c3" />
+
   </div>
 </template>
 
@@ -12,7 +17,20 @@ export default {
   name: 'Board',
   data () {
     return {
-
+      x: 0,
+      y: 0
+    }
+  },
+  created: function () {
+    window.addEventListener('keyup', this.up)
+  },
+  methods: {
+    up: function () {
+      console.log('key up event')
+      this.x++
+    },
+    click: function () {
+      this.y++
     }
   }
 }
