b673b4437b695383d0be85064efd1f64bbbfeffb
John Nicholas <themrtortoise@gmail.com>
2018-05-10 23:12:11 +0100

    can move up, combines same numbers into double

--- src/game/moves.js
+++ src/game/moves.js
@@ -1,17 +1,53 @@
-function moveUp (state) {
-  console.log('up')
-  function isNotAtTop (number) {
-    return number.y !== 0
-  }
-  for (let i = 0; i < state.numbers.length; i++) {
-    const number = state.numbers[i]
-    if (isNotAtTop(number)) {
-      // let numbersAbove = numbers.filter(n => n.x == number.x && n.y < number.y);
-      number.y = 0
-    }
+export const moveUp = function (state) {
+  // console.log('up')
+
+  function numberAtTop (number) {
+    return number.y === 0
   }
 
+  function numberAboveNumber (number, numbers) {
+    return numbers.some(n => n.x === number.x && n.y === number.y - 1)
+  }
+
+  function moveNumberUp (number, numbers) {
+    if (numberAtTop(number)) return number
+
+    if (numberAboveNumber(number, numbers)) return number
+
+    const num = {x: number.x, y: number.y - 1, n: number.n}
+    return moveNumberUp(num, numbers)
+  }
+
+  const numbers = state.numbers.sort(compareY)
+  const newNumbers = []
+  numbers.forEach(number => {
+    // console.log(`processing number ${JSON.stringify(number)}; newNumbers: ${JSON.stringify(newNumbers)} `)
+    const newNumber = moveNumberUp(number, newNumbers)
+
+    if (newNumber.y !== number.y) {
+      // console.log(`moved number is not on same row ${JSON.stringify(newNumber)}`)
+      number.y = newNumber.y
+    }
+
+    // console.log(`new number is ${JSON.stringify(number)}`)
+
+    const numberAbove = newNumbers.find(n => n.y === number.y - 1)
+    if (numberAbove && numberAbove.n === number.n) {
+      numberAbove.n = numberAbove.n * 2
+    } else {
+      newNumbers.push(number)
+    }
+  })
+
+  state.numbers = newNumbers
+
   updateBoard(state)
+}
+
+function compareY (a, b) {
+  if (a.y < b.y) return -1
+  if (a.y > b.y) return 1
+  return 0
 }
 
 function updateBoard (state) {
@@ -25,8 +61,4 @@ function updateBoard (state) {
       }
     }
   }
-}
-
-export default {
-  moveUp
 }
--- test/unit/specs/boardMoves.spec.js
+++ test/unit/specs/boardMoves.spec.js
@@ -1,12 +1,27 @@
-import moves from '@/game/moves.js'
-const {moveUp} = moves
+import {moveUp} from '@/game/moves.js'
+import state from '@/game/gameState.js'
 
 describe('moveUp will', () => {
-  it('move a 2 to the top when called', () => {
-    const state = {
-      numbers: [{n: 2, x: 0, y: 1}]
-    }
+  it('leave a 2 alone if already at top on own', () => {
+    state.numbers = [{n: 2, x: 0, y: 0}]
     moveUp(state)
-    expect(state.numbers[0].y).toEqual(0)
+    expect(state.board[0][0]).toEqual(2)
+  })
+
+  it('move a 2 to the top when called', () => {
+    state.numbers = [{n: 2, x: 0, y: 1}]
+    moveUp(state)
+    expect(state.board[1][0]).toEqual('')
+    expect(state.board[0][0]).toEqual(2)
+  })
+
+  it(`turn top 2 into a 4 if there are 2 adjacent on same column`, () => {
+    state.numbers = [
+      {n: 2, x: 0, y: 1},
+      {n: 2, x: 0, y: 0}
+    ]
+
+    moveUp(state)
+    expect(state.board[0][0]).toEqual(4)
   })
 })
